c5998ae9e072e289bf3cbf2c4a1aaaaad10f8d0a
Katarina Blažević <katarina.blazevic@iusinfo.hr>
2023-12-12 13:24:01 +0100

    Add SQL data access and Model classes, also ignore obj and bin

---
+++ HotelProject/HotelAppLibrary/Databases/ISQLDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace HotelAppLibrary.Databases
+{
+    internal interface ISQLDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName, bool isStoredProcedure = false);
+        void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoredProcedure = false);
+    }
+}
