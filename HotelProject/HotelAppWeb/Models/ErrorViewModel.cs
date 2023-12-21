4c1a0fd91432ffc9328d8664ab8cd7630483a5ae
Katarina Blažević <katarina.blazevic@iusinfo.hr>
2023-12-21 16:19:16 +0100

    Add HotelAppWeb project

---
+++ HotelProject/HotelAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HotelAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
